6916853c9bdcbbc3cbaee249a8afecbf63d69eb3
Tianyi Song <42670338+tysg@users.noreply.github.com>
2021-04-19 17:50:14 +0800

    Add git commands

--- git.sh
+++ git.sh
@@ -3,5 +3,11 @@ git config --global alias.co checkout
 git config --global alias.br branch
 git config --global alias.ci commit
 git config --global alias.st status
+git config --global alias.pl pull
+git config --global alias.ps push
+git config --global alias.pushup 'push --set-upstream origin $(git symbolic-ref --short HEAD)"'
+git config --global alias.pm 'pull --rebase=interactive origin master'
+git config --global alias.pmm 'pull --rebase=interactive origin main'
+
 
 
