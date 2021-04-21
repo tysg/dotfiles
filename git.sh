2f5eaf2d289f2b7a0d2c026e2b002e18258def41
Tianyi Song <42670338+tysg@users.noreply.github.com>
2021-04-21 14:36:01 +0800

    Correct git-pushup

--- git.sh
+++ git.sh
@@ -5,7 +5,7 @@ git config --global alias.ci commit
 git config --global alias.st status
 git config --global alias.pl pull
 git config --global alias.ps push
-git config --global alias.pushup 'push --set-upstream origin $(git symbolic-ref --short HEAD)"'
+git config --global alias.pushup '!git push --set-upstream origin $(git symbolic-ref --short HEAD)'
 git config --global alias.pm 'pull --rebase=interactive origin master'
 git config --global alias.pmm 'pull --rebase=interactive origin main'
 
